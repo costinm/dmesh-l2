895b0905590e90e715c39b90c875da98a2585f90
costin <costin@gmail.com>
2020-07-25 22:44:33 -0700

    Adjustments.

--- cmd/dmesh/dmesh.go
+++ cmd/dmesh/dmesh.go
@@ -2,110 +2,50 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/costinm/dmesh-l2/pkg/lmnet"
 	"github.com/costinm/dmesh-l2/pkg/netstacktun"
-	"github.com/costinm/wpgate/pkg/auth"
+	"github.com/costinm/wpgate/pkg/bootstrap"
 	"github.com/costinm/wpgate/pkg/conf"
 	"github.com/costinm/wpgate/pkg/h2"
 	"github.com/costinm/wpgate/pkg/mesh"
 	"github.com/costinm/wpgate/pkg/msgs"
-	"github.com/costinm/wpgate/pkg/transport/httpproxy"
 	"github.com/costinm/wpgate/pkg/transport/local"
-	sshgate "github.com/costinm/wpgate/pkg/transport/ssh"
 	uds2 "github.com/costinm/wpgate/pkg/transport/uds"
-	"github.com/costinm/wpgate/pkg/ui"
 )
 
-// Android and device version of DMesh.
+// Full: all features. Has a UDS connection, similar with the Android package.
 func main() {
 	log.Print("Starting native process pwd=", os.Getenv("PWD"), os.Environ())
-
-	// SYSTEMSERVERCLASSPATH=/system/framework/services.jar:/system/framework/ethernet-service.jar:/system/framework/wifi-service.jar:/system/framework/com.android.location.provider.jar
-	// PATH=/sbin:/system/sbin:/system/bin:/system/xbin:/odm/bin:/vendor/bin:/vendor/xbin
-	// STORAGE=/storage/emulated/0/Android/data/com.github.costinm.dmwifi/files
-	// ANDROID_DATA=/data
-	// ANDROID_SOCKET_zygote_secondary=12
-	// ASEC_MOUNTPOINT=/mnt/asec
-	// EXTERNAL_STORAGE=/sdcard
-	// ANDROID_BOOTLOGO=1
-	// ANDROID_ASSETS=/system/app
-	// BASE=/data/user/0/com.github.costinm.dmwifi/files
-	// ANDROID_STORAGE=/storage
-	// ANDROID_ROOT=/system
-	// DOWNLOAD_CACHE=/data/cache
-	// BOOTCLASSPATH=/system/framework/core-oj.jar:/system/framework/core-libart.jar:/system/framework/conscrypt.jar:/system/framework/okhttp.jar:/system/framework/bouncycastle.jar:/system/framework/apache-xml.jar:/system/framework/ext.jar:/system/framework/framework.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/ims-common.jar:/system/framework/android.hidl.base-V1.0-java.jar:/system/framework/android.hidl.manager-V1.0-java.jar:/system/framework/framework-oahl-backward-compatibility.jar:/system/framework/android.test.base.jar:/system/framework/com.google.vr.platform.jar]
-
-	cfgf := os.Getenv("BASE")
-	if cfgf == "" {
-		cfgf = os.Getenv("HOME")
-		if cfgf == "" {
-			cfgf = os.Getenv("TEMPDIR")
-		}
-		if cfgf == "" {
-			cfgf = os.Getenv("TMP")
-		}
-		if cfgf == "" {
-			cfgf = "/tmp"
-		}
+	bp := 5200
+	base := os.Getenv("BASE_PORT")
+	if base != "" {
+		bp, _ = strconv.Atoi(base)
 	}
 
-	cfgf += "/"
-
-	// File-based config
-	config := conf.NewConf(cfgf)
-
-	meshH := auth.Conf(config, "MESH", "v.webinf.info:5222")
-
-	// Init or load certificates/keys
-	authz := auth.NewAuth(config, os.Getenv("HOSTNAME"), "v.webinf.info")
-	msgs.DefaultMux.Auth = authz
-
-	// HTTPGate - common structures
-	GW := mesh.New(authz, nil)
-
-	// SSH transport + reverse streams.
-	sshg := sshgate.NewSSHGate(GW, authz)
-	GW.SSHGate = sshg
-	sshg.InitServer()
-	sshg.ListenSSH(":5222")
-
-	// Connect to a mesh node
-	if meshH != "" {
-		GW.Vpn = meshH
-		go sshgate.MaintainVPNConnection(GW)
+	cfgDir := os.Getenv("HOME") + "/.ssh/"
+	all := &bootstrap.ServerAll{
+		ConfDir:  cfgDir,
+		BasePort: bp,
 	}
+	bootstrap.StartAll(all)
 
-	// Local discovery interface - multicast, local network IPs
-	ld := local.NewLocal(GW, authz)
-	go ld.PeriodicThread()
+	// Debug interface
+	log.Println("Starting WPS server on ", all.BasePort)
 
-	h2s, err := h2.NewTransport(authz)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	initUDSConnection(h2s, GW, ld, config)
-
-	hgw := httpproxy.NewHTTPGate(GW, h2s)
-	hgw.HttpProxyCapture("localhost:5204")
-
-	// Start a basic UI on the debug port
-	u, _ := ui.NewUI(GW, h2s, hgw, ld)
+	initUDSConnection(all.H2, all.GW, all.Local, all.Conf)
 
 	//// Periodic registrations.
 	//m.Registry.RefreshNetworksPeriodic()
 
-	log.Printf("Loading with VIP6: %v ID64: %s %s\n", h2s.VIP6,
-		base64.RawURLEncoding.EncodeToString(h2s.Certs.VIP6[8:]))
-
-	http.ListenAndServe("localhost:5227", u)
+	http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", all.BasePort+bootstrap.HTTP_DEBUG), all.UI)
 }
 
 func initUDSConnection(h2 *h2.H2, gw *mesh.Gateway, ld *local.LLDiscovery, cfg *conf.Conf) {
